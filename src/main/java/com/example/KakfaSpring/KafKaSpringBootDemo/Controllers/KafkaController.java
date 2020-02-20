387bab4c8e2f50ed60f338da0462e4ff5b93e550
Saurabh <saurabh.verma.miet@gmail.com>
2020-02-20 11:58:55 +0530

    Added producer config using kafka template

--- src/main/java/com/example/KakfaSpring/KafKaSpringBootDemo/Controllers/KafkaController.java
+++ src/main/java/com/example/KakfaSpring/KafKaSpringBootDemo/Controllers/KafkaController.java
@@ -4,6 +4,8 @@ import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,32 +14,21 @@ import java.util.Properties;
 
 @RestController()
 public class KafkaController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
     private static Logger logger = LoggerFactory.getLogger(KafkaController.class);
 
     @PostMapping(value = "/produce")
     public String produce(@PathParam(value = "topic") String topic, @PathParam(value = "value") String value) {
-        Properties prop = new Properties();
-        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        //create producer
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(prop);
-
-        //producer record
-        ProducerRecord<String, String> prodrRecord =
-                new ProducerRecord<String, String>(topic, value);
-
-        //send data
-        producer.send(prodrRecord);
-        producer.flush();
-        producer.close();
-
+        kafkaTemplate.send(topic, value);
+        kafkaTemplate.flush();
         return "Data produced";
     }
 
     @PostMapping(value = "/produceWithCallback")
-    public String produceWithCallback(String toipc, String value) {
+    public String produceWithCallback(@PathParam(value = "topic") String topic, @PathParam(value = "value") String value) {
         Properties prop = new Properties();
         prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -48,9 +39,11 @@ public class KafkaController {
         //producer record
 
         ProducerRecord<String, String> prodrRecord =
-                new ProducerRecord<String, String>(toipc, value);
+                new ProducerRecord<String, String>(topic, value);
         //send data
-
+        kafkaTemplate.send(topic, value);
+        kafkaTemplate.send(topic, value);
+        kafkaTemplate.flush();
         producer.send(prodrRecord, new Callback() {
             public void onCompletion(RecordMetadata recordMetadata, Exception e) {
                 if (e == null) {
@@ -70,5 +63,4 @@ public class KafkaController {
         producer.close();
         return "Data produced with feedback";
     }
-
 }
